8ae67408d215e4aa98c9961f8143ddbf58f036cd
Juan Pablo <57115669+juanpisuribe13@users.noreply.github.com>
2020-11-23 20:20:33 -0500

    Cursor bug fix and improved syntax (v.1.1.1)
    - Improved the syntax as best as I could
    - Permanent wait cursor after clicking the button has been fixed
    - Removed the dev branch(?) (that was a horrible idea to be honest)
    
    This may be the last update that I'll do unless if someone either creates an issue or sends a pull request. Please refer to readme.md

--- src/com/juanpis/twtAPI.java
+++ src/com/juanpis/twtAPI.java
@@ -1,24 +1,13 @@
 package com.juanpis;
 
-import javax.swing.JOptionPane;
 import twitter4j.TwitterException;
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.Status;
-import twitter4j.api.TweetsResources;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
 
 public class twtAPI {
-    
     public Object tweet;
-
-    private static boolean isTweeted;
-    private static String tweetSuccessful = ("Tweet has been posted successfully.");
-    private static String noConnectionError = ("No internet connection available, " +
-            "please connect to the internet and try again.");
-
     public static boolean isTweetMaximum(int t) {
         if (t < 280) {
             return false;
@@ -27,20 +16,16 @@ public class twtAPI {
         }
     }
     
-    public static String tweet(String CS, String CSS, String AT, String AST, String post) { // why
-
+    public static void tweet(String CS, String CSS, String AT, String AST, String post) { // why
         Main GUI = new Main();
-        
         ConfigurationBuilder OATH = new ConfigurationBuilder();
         OATH.setDebugEnabled(true)
                 .setOAuthConsumerKey(CS)
                 .setOAuthConsumerSecret(CSS)
                 .setOAuthAccessToken(AT)
                 .setOAuthAccessTokenSecret(AST);
-
         TwitterFactory tf = new TwitterFactory(OATH.build());
         Twitter twt = tf.getInstance();
-    
 
         try {
             Status status = twt.updateStatus(post);
@@ -48,11 +33,5 @@ public class twtAPI {
         } 
         catch (TwitterException name) {
             GUI.errorSignal();
-        } finally {
-            return "Finished";
-        }
-
-        }
-
-
+        } }
 }
